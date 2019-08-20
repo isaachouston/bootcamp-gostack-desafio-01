d772a915aae1c50659723b7f4e6c6e07e1df7b2f
isaachouston <isaacprh@gmail.com>
2019-08-19 21:11:06 -0300

    Desafio 01, conceitos do NodeJs

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+let request = 0;
+
+const projects = [];
+
+function checkIdExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+  if (!project) {
+    return res.status(400).json({ error: "ID does not exists" });
+  }
+  return next();
+}
+
+function checkRequest(req, res, next) {
+  request++;
+
+  console.log(`Quantidade requisição: ${request}`);
+
+  next();
+}
+server.use(checkRequest);
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
+server.post("/project/:id/tasks", checkIdExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.put("/projects/:id", checkIdExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", checkIdExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.map(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+server.listen(3333);
